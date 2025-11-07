8c1d3224285ddd4c1d0a2e9931abb916711636a0
yukiko-shi <202421090428@stu.zuel.edu.cn>
2025-11-07 15:52:37 +0800

    fix(cleaners): the problem of replace_in_column function

--- tidymut/cleaners/cdna_proteolysis_cleaner.py
+++ tidymut/cleaners/cdna_proteolysis_cleaner.py
@@ -16,7 +16,7 @@ from .basic_cleaners import (
     validate_mutations,
     average_labels_by_name,
     convert_to_mutation_dataset_format,
-    replace_in_protein_name,
+    replace_in_column,
 )
 from .cdna_proteolysis_custom_cleaners import (
     validate_wt_sequence,
@@ -240,10 +240,10 @@ def create_cdna_proteolysis_cleaner(
                 in_place=True,
             )
             .delayed_then(
-                replace_in_protein_name,
-                old = ".pdb",
+                replace_in_column,
                 name_column=final_config.column_mapping.get("WT_name", "WT_name"),
-            )
+                old = ".pdb",
+            )    
             .delayed_then(
                 convert_to_mutation_dataset_format,
                 name_column=final_config.column_mapping.get("WT_name", "WT_name"),
